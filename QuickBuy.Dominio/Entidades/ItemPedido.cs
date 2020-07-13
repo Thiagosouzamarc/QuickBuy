9ed6134fa0d2dd579eff0ce821a2f4f2316eb7be
thiago souza marciano <thiagosk8_souza@hotmail.com>
2020-07-13 19:38:09 -0300

    Criação das entidades

---
+++ QuickBuy.Dominio/Entidades/ItemPedido.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class ItemPedido
+    {
+        public int Id { get; set; }
+        public string ProdutoId { get; set; }
+        public string Quantidade { get; set; }
+
+        /// <summary>
+        /// Um Usuario Pode ter nenhum ou muitos pedidos
+        /// </summary>
+
+        public ICollection<Pedido> Pedidos { get; set; }
+    }
+}
